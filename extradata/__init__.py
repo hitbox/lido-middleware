5a50130bf6b7a3d902e5b259b4079f5364016ba4
Carl Harris <elgoogemail2007@gmail.com>
2021-02-03 03:05:37 -0500

    sort class/instance names, remove unused method

--- extradata/__init__.py
+++ extradata/__init__.py
@@ -7,7 +7,39 @@ class ExtradataError(Exception):
     pass
 
 
+class AircraftRegistrationAirlineMapping:
+    """
+    Map aircraft registration (tail) to airline.
+    """
+
+    def __init__(self):
+        import warnings
+        warnings.warn('Temporary defaultdict implementation')
+        self.airline = defaultdict(lambda:'ABX')
+
+
+class AirlineDesignators:
+    """
+    Access to company/airline designator mappings in both directions.
+
+    airline_designators.by_company[company] -> airline code
+    airline_designators.by_airline[airline] -> company code
+    """
+
+    def __init__(self):
+        path = Path(__file__).parent / 'airline_designators.csv'
+        with open(path, newline='') as fp:
+            self.by_company = dict(csv.reader(fp))
+            self.by_airline = {v: k for k, v in self.by_company.items()}
+            self.company_codes = list(self.by_company)
+            self.airline_codes = list(self.by_airline)
+            self.all_codes = self.company_codes + self.airline_codes
+
+
 class Stations:
+    """
+    Mappings to-from iata/icao.
+    """
 
     _station_types = ['iata', 'icao']
     _opposite_station_type = {'iata': 'icao', 'icao': 'iata'}
@@ -45,52 +77,7 @@ class Stations:
         else:
             return {'iata': self.icao2iata[station], 'icao': station}
 
-    def update_other_station_by_type(self, data, prefix, sep='_'):
-        """
-        Update the IATA/ICAO station that is not already set, INPLACE. Both may
-        not be set.
-        """
-        source_keys = [prefix + sep + postfix for postfix in self._station_types]
-        values = [data[source_key] for source_key in source_keys]
-        if all(values):
-            raise ExtradataError('Both types of stations already set')
-        for station_type in self._station_types:
-            value = data.get(prefix + sep + station_type)
-            if value is not None:
-                lookup = self._opposite_station_lookup[station_type]
-                opposite_type = self._opposite_station_type[station_type]
-                data[prefix + sep + opposite_type] = lookup[value]
 
-
-class AirlineDesignators:
-    """
-    Access to company/airline designator mappings in both directions.
-
-    airline_designators.by_company[company] -> airline code
-    airline_designators.by_airline[airline] -> company code
-    """
-
-    def __init__(self):
-        path = Path(__file__).parent / 'airline_designators.csv'
-        with open(path, newline='') as fp:
-            self.by_company = dict(csv.reader(fp))
-            self.by_airline = {v: k for k, v in self.by_company.items()}
-            self.company_codes = list(self.by_company)
-            self.airline_codes = list(self.by_airline)
-            self.all_codes = self.company_codes + self.airline_codes
-
-
-class AircraftRegistrationAirlineMapping:
-    """
-    Map aircraft registration to airline.
-    """
-
-    def __init__(self):
-        import warnings
-        warnings.warn('Temporary defaultdict implementation')
-        self.airline = defaultdict(lambda:'ABX')
-
-
-stations = Stations()
+aircraftregistration = AircraftRegistrationAirlineMapping()
 airline_designators = AirlineDesignators()
-aircraftregistration2airline = AircraftRegistrationAirlineMapping()
+stations = Stations()
