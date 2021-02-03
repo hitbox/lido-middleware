8a54e63e0c4dd9da74287d2bd7aba85e1bb7caff
Carl Harris <elgoogemail2007@gmail.com>
2021-02-03 03:08:50 -0500

    sort classes added missing tests

--- tests/test_extradata.py
+++ tests/test_extradata.py
@@ -1,34 +1,38 @@
 import unittest
 
 from extradata import ExtradataError
+from extradata import aircraftregistration
+from extradata import airline_designators
 from extradata import stations
 
-class TestExtradata(unittest.TestCase):
+class TestAircraftRegistrationAirlineMapping(unittest.TestCase):
 
-    def test_stations_detect_code_type(self):
+    @unittest.skip('mapping will come from netline/base database')
+    def test_airline(self):
+        pass
+
+
+class TestAirlineDesignators(unittest.TestCase):
+
+    def test_by_company(self):
+        self.assertEqual(airline_designators.by_company['ABX'], 'GB')
+
+    def test_by_airline(self):
+        self.assertEqual(airline_designators.by_airline['GB'], 'ABX')
+
+
+class TestStations(unittest.TestCase):
+
+    def test_detect_code_type(self):
         self.assertEqual(stations.detect_code_type('KNGU'), 'icao')
         self.assertEqual(stations.detect_code_type('NGU'), 'iata')
         #
         with self.assertRaises(ExtradataError):
             stations.detect_code_type('NOPE')
 
-    def test_stations_update_other_station_by_type(self):
-        # sets icao
-        d = dict(destination_iata='NGU', destination_icao=None)
-        stations.update_other_station_by_type(d, 'destination')
-        self.assertEqual(d, dict(destination_iata='NGU', destination_icao='KNGU'))
-        # sets iata
-        d = dict(destination_iata=None, destination_icao='KNGU')
-        stations.update_other_station_by_type(d, 'destination')
-        self.assertEqual(d, dict(destination_iata='NGU', destination_icao='KNGU'))
-        # raise for key not exist
-        with self.assertRaises(KeyError):
-            stations.update_other_station_by_type(dict(), 'destination')
-        # raise for both set
-        with self.assertRaises(ExtradataError):
-            stations.update_other_station_by_type(
-                dict(destination_iata='NGU', destination_icao='KNGU'),
-                'destination')
+    def test_filled(self):
+        self.assertEqual(stations.filled('NGU'), {'iata': 'NGU', 'icao': 'KNGU'})
+        self.assertEqual(stations.filled('KNGU'), {'iata': 'NGU', 'icao': 'KNGU'})
 
 
 if __name__ == '__main__':
