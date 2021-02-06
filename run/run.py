7b102ef8ba74093490ccde28003c9aced6c39d80
Carl Harris <elgoogemail2007@gmail.com>
2021-02-06 07:11:16 -0500

    more complete run function

--- run/run.py
+++ run/run.py
@@ -1,36 +1,30 @@
-import ftplib
-import io
 import logging
 
-from datetime import date
-from datetime import timedelta
-from pprint import pformat
-
 from marshmallow import ValidationError
-from imap_tools import AND
-from imap_tools import MailBox
 
 from lido import LIDOWeightBalanceMessage
-
-from utils import _resolve
-from utils import cleanlines
-from utils import hide
 
 def run(conf):
     logger = logging.getLogger(__name__)
     schema = conf.schema_class()
 
     for msg in conf.source.itermessages():
-        message_data = conf.message_processor(msg)
         try:
-            loadplan = schema.load(message_data)
-        except ValidationError as error:
-            logger.exception(error)
-            for key, messages in error.messages.items():
-                for message in messages:
-                    logger.error(f'{key} {message} ({message_data[key]!r})')
+            message_data = conf.message_processor(msg)
+        except Exception as e:
+            logger.exception(e)
         else:
-            lido_message = LIDOWeightBalanceMessage(loadplan)
-            lido_string = str(lido_message)
-            logger.info(lido_string)
+            try:
+                loadplan = schema.load(message_data)
+            except ValidationError as error:
+                logger.exception(error)
+                for key, messages in error.messages.items():
+                    if not key.startswith('_'):
+                        for message in messages:
+                            logger.error(f'{key} {message} ({message_data[key]!r})')
+            except Exception as e:
+                logger.exception(e)
+            else:
+                lido_message = LIDOWeightBalanceMessage(loadplan)
+                conf.output.write(lido_message)
 
