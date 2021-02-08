9eb65552a872b40b19b6735941da3018d2a8f1b5
Carl Harris <elgoogemail2007@gmail.com>
2021-02-08 04:21:57 -0500

    added missing message filter and archiver. added logging

--- run/run.py
+++ run/run.py
@@ -9,16 +9,23 @@ def run(conf):
 
     source = conf['SOURCE']
     schema_class = conf['SCHEMA_CLASS']
+    message_filter = conf['MESSAGE_FILTER']
     message_processor = conf['MESSAGE_PROCESSOR']
     output = conf['OUTPUT']
+    message_archive = conf['MESSAGE_ARCHIVE']
 
     schema = schema_class()
-    for msg in source.itermessages():
+    for message in source.itermessages():
+        if not message_filter.filter(message):
+            logger.info('message filter rejected %r', message)
+            continue
+        logger.info('message %r', message)
         try:
-            message_data = message_processor(msg)
+            message_data = message_processor(message)
         except Exception as e:
             logger.exception(e)
         else:
+            logger.info('message_data %r', message_data)
             try:
                 loadplan = schema.load(message_data)
             except ValidationError as error:
@@ -30,5 +37,9 @@ def run(conf):
             except Exception as e:
                 logger.exception(e)
             else:
+                logger.info('loadplan %r', loadplan)
                 lido_message = LIDOWeightBalanceMessage(loadplan)
+                logger.info('lido_message %r', lido_message)
                 output.write(lido_message)
+                message_archive.save(message)
+                logger.info('saved %r', message)
