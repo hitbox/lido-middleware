35e008a1636975c799e14c14951d05dd26cf6e26
Carl Harris <elgoogemail2007@gmail.com>
2021-02-11 01:51:54 -0500

    add watch-like infinite, sleep-interval loop

--- run/main.py
+++ run/main.py
@@ -1,6 +1,7 @@
 import argparse
 import configparser
 import logging.config
+import time
 
 from pathlib import Path
 
@@ -10,18 +11,31 @@ from .run import run
 
 def main(argv=None):
     """
-    Download, parse/extract and write LIDO weight and balance message output.
+    Command line interface to `run.run`.
     """
-    parser = argparse.ArgumentParser(description=main.__doc__, prog='run')
+    parser = argparse.ArgumentParser(description=run.__doc__, prog='run')
     parser.add_argument('pyfile', type=Path)
+    parser.add_argument(
+        '-w', '--watch', type=int, metavar='N',
+        help='Run repeatedly every %(metavar)s seconds.')
     args = parser.parse_args(argv)
 
+    # do logging in config or this takes over
     logging.basicConfig(level=logging.INFO, style='{')
     logger = logging.getLogger(__name__)
 
     conf = pyfile_config(args.pyfile)
-    # run logging exceptions
+
+    exit_code = 1
     try:
-        return run(conf)
-    except:
-        logger.exception('An exception occurred')
+        while True:
+            # run, logging exceptions
+            exit_code = run(conf)
+            if args.watch is None:
+                break
+            else:
+                logger.info('sleeping %s', args.watch)
+                time.sleep(args.watch)
+    except KeyboardInterrupt:
+        pass
+    return exit_code
