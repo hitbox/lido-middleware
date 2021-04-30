88357bf286eb02bebd668181387425f629283393
Carl Harris <elgoogemail2007@gmail.com>
2021-04-30 03:06:32 -0400

    added initial outlook .eml file parsing

--- sable/main.py
+++ sable/main.py
@@ -1,4 +1,5 @@
 import argparse
+import email
 import pickle
 
 from pathlib import Path
@@ -14,6 +15,7 @@ def main(argv=None):
     parser = argparse.ArgumentParser(description=main.__doc__)
     parser.add_argument('-f', '--file', nargs='+', type=argparse.FileType())
     parser.add_argument('-p', '--pickle', nargs='+')
+    parser.add_argument('-e', '--eml', nargs='+', help='email.message_from_file')
     parser.add_argument('--html', default='-', nargs='?', type=argparse.FileType('w'))
     args = parser.parse_args(argv)
 
@@ -37,6 +39,20 @@ def main(argv=None):
                 loadplan_data = extract.from_text(text)
                 loadplan = loadplanschema.load(loadplan_data)
                 loadplans.append(loadplan)
+    elif args.eml:
+        for fn in args.eml:
+            with open(fn, 'rb') as fp:
+                message = email.message_from_bytes(fp.read())
+                for part in message.walk():
+                    print(fn)
+                    print(part.get_content_type())
+                    print(part.get_content_charset())
+                    #if part.get_content_type() == 'text/plain':
+                    #    print(fn)
+                    #    print(part.get_payload())
+                    #print([item for item in part.items() if 'name' in item[0].lower()])
+                    if part.get_content_disposition() == 'attachment':
+                        pass
 
     if args.html:
         path = Path(__file__).parent / 'loadplan_table.html'
