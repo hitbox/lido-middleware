88737003365fc5536c41e231be73efee2b3fe620
Carl Harris <elgoogemail2007@gmail.com>
2021-06-11 03:15:40 -0400

    add note

--- wsiweather/parse.py
+++ wsiweather/parse.py
@@ -6,6 +6,7 @@ def taf_filename(fn):
     """
     # TAF: Terminal Aerodrome Forecasts
     # first two and last are unknown to me at this time
+    # also this is not used.
     _, _, dtstr, _ = fn.split('.')
     dt = datetime.strptime(dtstr, '%Y%m%dT%H%M%S')
     return (None, None, dt, None)
