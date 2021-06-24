5242aa5bff03045df26b0d03dae1f9be97ce8170
Carl Harris <elgoogemail2007@gmail.com>
2021-06-24 07:32:32 -0400

    sablev2 -> sable2

--- sable2/webdemo.py
+++ sable2/webdemo.py
@@ -1,4 +1,5 @@
 import pickle
+import traceback
 
 from flask import Blueprint
 from flask import Flask
@@ -6,8 +7,8 @@ from flask import current_app
 from flask import render_template
 
 from lido import LIDOWeightBalanceMessage
-from sablev2.extract import loadplan_from_message
-from sablev2.schema import LoadPlanSchema
+from sable2 import extract
+from sable2 import schema
 
 demo_bp = Blueprint('demo', __name__)
 
@@ -36,17 +37,42 @@ def index():
 
 @demo_bp.route('/<int:index>')
 def process(index):
-    email = messages[index]
-    demo = Demo()
-    demo.build_from_email(email)
-    template = 'show_processed.html'
-    return render_template(template, demo=demo)
+    message = messages[index]
+
+    extract_error = False
+    _schema = ''
+    _lido = ''
+    try:
+        _extract = extract.loadplan_from_message(message)
+    except:
+        extract_error = True
+        _extract = traceback.format_exc()
+    else:
+        try:
+            _schema = schema.LoadPlanSchema().load(_extract)
+        except:
+            _schema = traceback.format_exc()
+        else:
+            try:
+                _lido = LIDOWeightBalanceMessage(_schema, bypass_length_check=True)
+            except:
+                _lido = traceback.format_exc()
+    context = dict(
+        message = message,
+        extract = _extract,
+        extract_error = extract_error,
+        schema = _schema,
+        lido = _lido,
+    )
+    return render_template('show_processed.html', **context)
 
 def create_app():
     app = Flask(__name__)
 
     app.config.from_envvar('SABLEV2')
     app.register_blueprint(demo_bp)
+
+    app.add_template_filter(repr)
 
     with open(app.config['MESSAGES_PICKLE'], 'rb') as fp:
         global messages
