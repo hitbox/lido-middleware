432d47d1a62bc2e86cd4e3e4bd7b6515561c6be4
Carl Harris <elgoogemail2007@gmail.com>
2021-06-24 07:33:28 -0400

    remove comment

--- pistol2/extract.py
+++ pistol2/extract.py
@@ -223,7 +223,6 @@ def loadplan_from_message(message):
     """
     Loadplan data from email message. Adds message data too.
     """
-    #text = message.attachments[0].payload.decode()
     loadplan_data = loadplan_from_text(message.text)
     loadplan_data['message_to'] = message.to
     return loadplan_data
