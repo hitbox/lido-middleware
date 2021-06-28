a88d8568213e3a7de85869ea120e1790b8e51c77
Carl Harris <elgoogemail2007@gmail.com>
2021-06-28 03:05:47 -0400

    copy button for lido message

--- sable2/static/site.js
+++ sable2/static/site.js
@@ -1,7 +1,7 @@
 "use strict";
 
 window.addEventListener("DOMContentLoaded", function() {
-    let toggles = document.querySelectorAll(".toggle[data-target]");
+    let toggles = document.querySelectorAll("button.toggle[data-target]");
     for (let i = 0; i < toggles.length; i++) {
         toggles[i].addEventListener("click", function() {
             let target = this.getAttribute("data-target");
@@ -13,4 +13,15 @@ window.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
+
+    let copyButtons = document.querySelectorAll("button.copy");
+    console.log(copyButtons);
+    for (let i = 0; i < copyButtons.length; i++) {
+        copyButtons[i].addEventListener("click", function() {
+            let source = this.getAttribute("data-source");
+            source = document.getElementById(source);
+            navigator.clipboard.writeText(source.innerHTML);
+        });
+    }
+
 });
