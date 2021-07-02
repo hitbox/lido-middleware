9f8a836bf8ba277bc24bd3c1a8704bf3eca7f887
Carl L. Harris <carl.harris@globalflightsource.com>
2021-07-02 03:24:04 -0400

    better naming

--- run/main.py
+++ run/main.py
@@ -13,7 +13,7 @@ def main(argv=None):
     Command line interface to `run.run`.
     """
     parser = argparse.ArgumentParser(description=run.__doc__, prog='run')
-    parser.add_argument('pyfile', type=Path)
+    parser.add_argument('config_pyfile', type=Path)
     parser.add_argument(
         '--shell',
         action='store_true',
@@ -24,7 +24,7 @@ def main(argv=None):
     logging.basicConfig(level=logging.INFO, style='{')
     logger = logging.getLogger(__name__)
 
-    conf = pyfile_config(args.pyfile)
+    conf = pyfile_config(args.config_pyfile)
 
     if args.shell:
         code.interact(local=conf)
