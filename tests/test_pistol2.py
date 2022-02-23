34a827664c7b31931cc1bc087dabf6e6477dfa90
Carl Harris <elgoogemail2007@gmail.com>
2022-02-23 05:32:27 -0500

    tests: optional printing

--- tests/test_pistol2.py
+++ tests/test_pistol2.py
@@ -6,6 +6,7 @@ from pistol2 import extract
 
 # download the inbox messages and save in a pickle as an unnamed list
 PSTLMESSAGES = os.environ.get('PSTLMESSAGES')
+PSTLMESSAGES_PRINT = os.environ.get('PSTLMESSAGES_PRINT')
 
 @unittest.skipIf(
     PSTLMESSAGES is None,
@@ -20,11 +21,16 @@ class TestExtract(unittest.TestCase):
     def test_run(self):
         # simply testing that only our internal errors are thrown.
         # would like to make better.
+        nerr = 0
         for msg in self.pstlmessages:
             try:
                 loadplan_data = extract.loadplan_from_message(msg)
             except extract.PistolExtractError as e:
-                print(f'pistol2.extract:accepting:{e!r}')
+                if PSTLMESSAGES_PRINT:
+                    print(f'pistol2.extract:accepting:{e!r}')
+                nerr += 1
+        if nerr > 0 and PSTLMESSAGES_PRINT:
+            print('Should only see PistolExtractError error messages.')
 
 
 if __name__ == '__main__':
--- tests/test_sable2.py
+++ tests/test_sable2.py
@@ -6,6 +6,7 @@ from sable2 import extract
 
 # download the inbox messages and save in a pickle as an unnamed list
 SABLEMESSAGES = os.environ.get('SABLEMESSAGES')
+SABLEMESSAGES_PRINT = os.environ.get('SABLEMESSAGES_PRINT')
 
 @unittest.skipIf(
     SABLEMESSAGES is None,
@@ -20,11 +21,16 @@ class TestExtract(unittest.TestCase):
     def test_run(self):
         # simply testing that only our internal errors are thrown.
         # would like to make better.
+        nerr = 0
         for msg in self.sablemessages:
             try:
                 loadplan_data = extract.loadplan_from_message(msg)
             except extract.SableExtractError as e:
-                print(f'sable2.extract:accepting:{e!r}')
+                if SABLEMESSAGES_PRINT:
+                    print(f'sable2.extract:accepting:{e!r}')
+                nerr += 1
+        if nerr > 0 and SABLEMESSAGES_PRINT:
+            print('Should only see SableExtractError error messages.')
 
 
 if __name__ == '__main__':
