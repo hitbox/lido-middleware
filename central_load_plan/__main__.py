25a638826b8cab37a2c1dbe236fa2b299ec10ed6
Carl Harris <elgoogemail2007@gmail.com>
2023-04-21 11:07:59 -0400

    consolidate main.py into cli.py

--- central_load_plan/__main__.py
+++ central_load_plan/__main__.py
@@ -1,8 +1,5 @@
 import sys
 
 from . import cli
-from . import main
 
-options = cli.parse_args()
-result = main.run(options)
-sys.exit(result)
+sys.exit(cli.run())
--- central_load_plan/cli.py
+++ central_load_plan/cli.py
@@ -1,6 +1,10 @@
 import argparse
+import smtplib
 
-from . import main
+from pprint import pprint
+
+from . import config
+from .app import CLPApp
 from .constants import APPNAME
 
 def parse_args(argv=None):
@@ -8,7 +12,7 @@ def parse_args(argv=None):
     Process XML files into CLP email messages and send.
     """
     parser = argparse.ArgumentParser(
-        description = main.run.__doc__,
+        description = run.__doc__,
         prog = APPNAME,
     )
     parser.add_argument('config', nargs='+')
@@ -30,3 +34,53 @@ def parse_args(argv=None):
     )
     options = parser.parse_args(argv)
     return options
+
+def _test_smtp(conf):
+    with smtplib.SMTP(**conf):
+        print(f'Connected SMTP {conf}')
+
+def _test_oracle(company_confs):
+    from .crewmember import get_engine
+    for company_code, company_dbconf in company_confs.items():
+        engine = get_engine(company_dbconf)
+        with engine.connect():
+            print(f'Connected {company_code} {engine}')
+
+def run():
+    """
+    Create Central Load Plan emails and files from XML files.
+    """
+    options = parse_args()
+    appconf = config.process(options.config)
+
+    # flags that stop after processed
+    if (
+        options.dump_config
+        or options.test_smtp
+        or options.test_oracle
+    ):
+        if options.dump_config:
+            pprint(appconf)
+
+        if options.test_smtp:
+            _test_smtp(appconf.smtpconf)
+
+        if options.test_oracle:
+            _test_oracle(appconf.dbconf)
+
+        return
+
+    clpapp = CLPApp(
+        appconf.source_glob,
+        appconf.move_to,
+        appconf.exception_move_to,
+        appconf.smtpconf,
+        appconf.emailconf,
+        appconf.file_output_conf,
+        appconf.dbconf,
+        appconf.ignore_crewmembers,
+        abort_on_error = appconf.abort_on_error,
+        dry_run = appconf.dry_run,
+    )
+
+    clpapp.run()
